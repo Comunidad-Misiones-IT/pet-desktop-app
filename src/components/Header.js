4afd90ebb297257441ea87981526da8891e2444a
Nataya Soledad <natayafs@gmail.com>
2021-04-22 00:24:17 -0300

    Creación de la componente Preferencias y configuración de rutas

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,19 @@ import InicioSesion from "./InicioSesion";
 import Registro from "./Registro";
 import Galeria from "./Galeria";
 import Nosotros from "./Nosotros";
+import Preferencias from "./Preferencias";
 
 export default function Header() {
   return (
     <>
       <header>
           <nav>
-            <ul><Link to="/">Inicio</Link></ul>
+            <ul><Link to="/">Home</Link></ul>
             <ul><Link to="/iniciosesion">Inicio Sesion</Link></ul>
             <ul><Link to="/registro">Registro</Link></ul>
             <ul><Link to="/galeria">Galería</Link></ul>
             <ul><Link to="/nosotros">Nosotros</Link></ul>
+            <ul><Link to="/preferencias">Preferencias</Link></ul>
           </nav>
       </header>
 
@@ -23,6 +25,7 @@ export default function Header() {
     <Route path="/registro" exact component={Registro} />
     <Route path="/galeria" exact component={Galeria} />
     <Route path="/nosotros" exact component={Nosotros} />
+    <Route path="/preferencias" exact component={Preferencias} />
     </>
   );
 }
