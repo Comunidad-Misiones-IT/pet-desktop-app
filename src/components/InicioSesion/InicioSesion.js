68c576e2d1425d90c17e7b1baad702aae5e82ff3
Florencia <florencia.granucci2407@25watts.com.ar>
2021-04-29 15:06:17 -0300

    Componente inicio de sesion 2

--- src/components/InicioSesion/InicioSesion.js
+++ src/components/InicioSesion/InicioSesion.js
@@ -1,38 +1,30 @@
-import './InicioSesion.css';
+import "./InicioSesion.css";
 
 export default function InicioSesion() {
   return (
-    <div className='root'>
-
-    <div className='container'>
-        <form className='form'>
-          <label className='labels'>
+    <div className="root">
+      <div className="container">
+        <form className="form">
+          <label className="labels">
             Usuario
-            <input className='inputs' type="text" name="user" />
+            <input className="inputs" type="text" name="user" />
           </label>
-          <label className='labels'>
+          <label className="labels">
             Contraseña
-            <input className='inputs' type="password" name="pass" />
+            <input className="inputs" type="password" name="pass" />
           </label>
           <div>
-          <input className='socialMedia' type="submit" value="Google" />
-          <input className='socialMedia' type="submit" value="Facebook" />
+            <input className="socialMedia" type="submit" value="Google" />
+            <input className="socialMedia" type="submit" value="Facebook" />
           </div>
-          <input className='submit' type="submit" value="Ingresar" />
-        <div className='link'><a href='/registro'>Crear usuario</a></div>
+          <input className="submit" type="submit" value="Ingresar" />
+          <div className="link">
+            <a href="/registro">Crear usuario</a>
+          </div>
         </form>
 
-      <div className='aside'></div>
-    </div>
+        <div className="aside"></div>
+      </div>
     </div>
   );
 }
-
-
-
-
-
-
-
-
-   
